4ead672440de81c317f239678b691b51a653e4a7
valentin <delo.biss13@gmail.com>
2021-08-31 17:41:56 +0300

    Java.создали калькулятор

--- src/geekbrains/lesson_1/hello_world/Main.java
+++ src/geekbrains/lesson_1/hello_world/Main.java
@@ -13,6 +13,27 @@ public class Main {
 //        System.out.println("a = " + a);
 
        // создаем калькулятор
-
+        System.out.println("Введите операцию");
+        System.out.println("1. Сложение");
+        System.out.println("2. Вычитание");
+        System.out.println("3. Умножение");
+        System.out.println("4. Деление");
+        Scanner scanner = new Scanner(System.in);
+        int operation = scanner.nextInt();
+        System.out.println("Введите первое число");
+        int a = scanner.nextInt();
+        System.out.println("Введите торое число");
+        int b = scanner.nextInt();
+        int result;
+        if (operation == 1) {
+            result = a + b;
+        } else if (operation == 2) {
+            result = a - b;
+        } else if (operation == 3) {
+            result = a * b;
+        } else {
+            result = a / b;
+        }
+        System.out.println("Результат = " + result);
     }
 }
