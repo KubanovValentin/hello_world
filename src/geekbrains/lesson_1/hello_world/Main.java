1ce615d2e460bbdedb27e60885b3bc0b71086e3e
valentin <delo.biss13@gmail.com>
2021-08-31 16:51:24 +0300

    Java

---
+++ src/geekbrains/lesson_1/hello_world/Main.java
@@ -0,0 +1,9 @@
+package geekbrains.lesson_1.hello_world;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("я буду изучать Java");
+    }
+}
